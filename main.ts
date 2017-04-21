61caab8a5c463c3995e757077f563e6d92ee42ee
Ibro <ibro@ibro.me>
2017-04-21 08:08:20 +0200

    using Observable.create()

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 
+let words = ['coding blast', 'badword', 'coding', 'blast'];
 
-let source = Observable.fromEvent(document, 'mousemove');
+let source = Observable.create(function observerFunction(observer: Observer<string>) {
+    for (let word of words) {
+        observer.next(word);
+    }
+
+    observer.complete();
+});
 
 source.subscribe(next, error, complete);
 

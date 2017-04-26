5764e857406118aa773df25d337fb50a074247c1
Ibro <ibro@ibro.me>
2017-04-26 08:39:00 +0200

    RxJS cold observable

--- main.ts
+++ main.ts
@@ -1,45 +1,11 @@
-import {Observable} from "rxjs";
+import {Observable} from 'rxjs';
 
-const apiEndpoint = 'https://jsonplaceholder.typicode.com/posts';
+let source = Observable
+    .interval(1000)
+    .take(6);
 
-let divPosts = document.getElementById('posts');
-let btnGetPosts = document.getElementById('btnGetPosts');
+source.subscribe(x => console.log('sub 1: ', x));
 
-let click = Observable.fromEvent(btnGetPosts, "click");
-
-
-function renderPosts(posts) {
-    posts.forEach(function (post) {
-        let divPost = document.createElement('div');
-        divPost.innerText = `id: ${post.id}, title: ${post.title}`;
-        divPosts.appendChild(divPost);
-    })
-}
-
-let source = click
-    .flatMap(e => fetchData(apiEndpoint))
-    .take(5);
-
-
-let subscribtion = source.subscribe(next, error, complete);
-
-function next(posts: any) {
-    console.log(posts);
-    renderPosts(posts);
-}
-
-function error(err: any) {
-    console.log('error: ', err);
-}
-
-function complete() {
-    console.log('complete');
-}
-
-export function fetchData(url: string) {
-    return Observable
-        .fromPromise(
-            fetch(url).then(r => r.json())
-        );
-}
-
+setTimeout(function () {
+    source.subscribe(x => console.log('                sub 2: ', x));
+}, 2700);

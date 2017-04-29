f3df9247e8487090445baa462b976c8abbfd3681
Ibro <ibro@ibro.me>
2017-04-29 05:55:25 +0200

    flatMap with Observable

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import {Observable} from 'rxjs';
 let source = Observable
     .interval(100) // interval starts from 0
     .take(5) // take first 5
-    .flatMap(n => [n * 1, n * 2, n * 3]);
+    .flatMap(n => Observable.of(n + 10));
 
 source.subscribe((value) => {
     console.log('in next. Value: ', value);
